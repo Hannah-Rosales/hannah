5b6b7cf9c5d181acad3e89be262d30e3c16ccccc
Calvin Berlandino <2023-2-02178@lpunetwork.edu.ph>
2024-10-19 12:24:29 +0800

    not done

--- Act4A.py
+++ Act4A.py
@@ -9,23 +9,6 @@ current_date = input("Enter Current Date: ")
 student_data = obj1.get_student_data(name, course, student_number, academic_year, current_date)
 
 
-while True:
-    subject = input("Enter subject name (or 'done' to finish): ")
-    if subject.lower() == 'done':
-        break
-    units = int(input(f"Enter units for {subject}: "))
-
-
-
-    # additional fees
-additional_fees = []
-while True:
-    fee = input("Enter additional fee (or 'done' to finish): ")
-    if fee.lower() == 'done':
-        break
-    additional_fees.append(float(fee))
-
-
 
 
 
